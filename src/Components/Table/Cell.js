08074dd07fc15b8bf6f09ebdaa6ccf9903b7a7ac
Ruslan <ruslanlyanov1999@gmail.com>
2023-07-14 19:43:25 +0300

    first commit

---
+++ src/Components/Table/Cell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Square.module.css'
+const Square = () => {
+    return (
+        <div className={classes.all}>
+
+        </div>
+    );
+};
+
+export default Square;
